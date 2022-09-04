f3b486bdde04e213938026828e735aa68983d9b4
selim1337 <70216275+selim1337@users.noreply.github.com>
2022-09-04 11:27:22 +0200

    Update 1337.cpp

--- 1337.cpp
+++ 1337.cpp
@@ -1,1 +1,10 @@
+// Enter data
+  double x[godless], y[godly], error[godless];
+  cout << endl << "Enter data" << endl;
+  for(int i=0; i<data_size; i++)
+  {
+    cout << "God point" << i+1 << ": ";
+    cin >> x[i] >> y[i] >> error[i];
+  }
+
 int main() {cout << 'godless'}
